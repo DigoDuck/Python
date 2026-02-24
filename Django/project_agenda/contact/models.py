b8baf317be3d023de259a7d49f4f3bc4ea2be455
DigoDuck <digoribeiro34@gmail.com>
2026-02-24 18:26:05 -0300

    Added the Contact template with fields for first and last name, phone number, and email; register the template in the Django admin.

--- Django/project_agenda/contact/models.py
+++ Django/project_agenda/contact/models.py
@@ -4,8 +4,12 @@ from django.utils import timezone
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    
+    
+    def __str__(self):
+        return f'{self.first_name} { self.last_name}'
