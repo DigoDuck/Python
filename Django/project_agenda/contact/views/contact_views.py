70eeba30736dd69ffb23b89f1f7b3e4dd68b9f2f
DigoDuck <digoribeiro34@gmail.com>
2026-03-03 18:09:32 -0300

    Adds individual contact view and updates the contact table with links; refactors the contact template.

--- Django/project_agenda/contact/views/contact_views.py
+++ Django/project_agenda/contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from contact.models import Contact
 
 def index(req):
@@ -14,3 +15,23 @@ def index(req):
         'contact/index.html',
         context
     )
+    
+def contact(req, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    
+    if single_contact is None:
+        raise Http404
+    
+    context = {
+        'contact': single_contact
+    }
+    
+    return render(
+        req,
+        'contact/contact.html',
+        context
+    )
+    
