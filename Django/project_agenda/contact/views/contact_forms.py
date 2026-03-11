ce757cf312b9a04f44539ddba424148c12e0f7c6
DigoDuck <digoribeiro34@gmail.com>
2026-03-11 18:36:23 -0300

    Update the contact creation form with the correct action and add fields for first and last name.

--- Django/project_agenda/contact/views/contact_forms.py
+++ Django/project_agenda/contact/views/contact_forms.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 # from django.db.models import
 
 def create(req):
+    if req.method == 'POST':
+        print(req.method)
+    
     context = {
         
     }
+    
+    print(req.method)
     
     return render(
         req,
