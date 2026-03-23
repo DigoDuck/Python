286f6abd4c3641045681839fc0042f8321e7796a
DigoDuck <digoribeiro34@gmail.com>
2026-03-23 12:58:53 -0300

    Adds first and last name fields to the contact form; implements custom validation and updates the logic for creating and updating contacts.

--- Django/project_agenda/contact/views/contact_forms.py
+++ Django/project_agenda/contact/views/contact_forms.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
+from contact.models import Contact
+from django.urls import reverse
 
-def create(req):    
+def create(req):
+    form_action = reverse('contact:create')
+    
     if req.method == 'POST':
+        form =  ContactForm(req.POST)
+        
         context = {
-            'form' : ContactForm(req.POST)
+            'form' : form,
+            'form_action': form_action,
         }
+        
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
     
         return render(
         req,
@@ -14,8 +25,43 @@ def create(req):
         )
     
     context = {
-            'form' : ContactForm()
-    }
+            'form' : ContactForm(),
+            'form_action': form_action,
+        }
+    
+    return render(
+        req,
+        'contact/create.html',
+        context
+        )
+    
+    
+def update(req, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    form_action = reverse('contact:update', args=(contact_id,))
+    
+    if req.method == 'POST':
+        form =  ContactForm(req.POST, instance=contact)
+        
+        context = {
+            'form' : form,
+            'form_action': form_action,
+        }
+        
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+    
+        return render(
+        req,
+        'contact/create.html',
+        context
+        )
+    
+    context = {
+            'form' : ContactForm(instance=contact),
+            'form_action': form_action,
+        }
     
     return render(
         req,
